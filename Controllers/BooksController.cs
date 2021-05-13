7034e454ff6f99821a6574bd8b3c29e73bd10af3
vivek <vivek@athenaeum.in>
2021-05-13 10:34:21 +0530

    Add project files.

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,50 @@
+﻿using LT.NETCore.Demo1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LT.NETCore.Demo1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        //  GET: get all
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new List<Book>());
+        }
+
+        //  GET: get with id
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(new Book { Id = id, Title = "Learning Asp.Net Core", Description = "Learner's book", Price = 500.50 });
+        }
+
+        //  POST
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            return Ok(new { Message = "Created", Payload = book });
+        }
+
+        //  PUT
+        [HttpPut]
+        public IActionResult Update(Book book)
+        {
+            return Ok(new { Message = "Update", Payload = book });
+        }
+
+        //  DELETE
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok(new { Message = $"Deleted successfully: {id}" });
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LT.NETCore.Demo1.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+    }
+}
