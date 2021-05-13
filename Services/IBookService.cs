137282a2dad64b51d38683f8b466976f3d1a8892
vivek <vivek@athenaeum.in>
2021-05-13 11:40:13 +0530

    done

---
+++ Services/IBookService.cs
@@ -0,0 +1,17 @@
+﻿using LT.NETCore.Demo1.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LT.NETCore.Demo1.Services
+{
+    public interface IBookService
+    {
+        IEnumerable<BookListViewModel> Get();
+        BookListViewModel Get(int id);
+        void Create(BookCreateViewModel viewModel);
+        void Update(BookUpdateViewModel viewModel);
+        void Delete(int id);
+    }
+}
