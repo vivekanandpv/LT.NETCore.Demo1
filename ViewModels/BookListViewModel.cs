00dcabbe99fa5e135bfc5e46300f2ccaf902349f
vivek <vivek@athenaeum.in>
2021-05-13 11:32:48 +0530

    done

---
+++ ViewModels/BookListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LT.NETCore.Demo1.ViewModels
+{
+    public class BookListViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ ViewModels/BookUpdateViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace LT.NETCore.Demo1.ViewModels
+{
+    public class BookUpdateViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+    }
+}
